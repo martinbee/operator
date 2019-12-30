06a0c11d7b03cf70b9e9d5299c02f1615970ca44
Martin <martbee88@gmail.com>
2019-12-30 10:51:01 -0600

    feat: add stub second page

--- App.js
+++ App.js
@@ -1,97 +1,42 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import { Button } from 'react-native-material-ui';
+import { COLOR, ThemeContext, getTheme } from 'react-native-material-ui';
+import { StatusBar } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Home from './screens/Home';
+import NumberOfCalls from './screens/NumberOfCalls';
 
-const Colors = {
-  primary: '#1292B4',
-  white: '#FFF',
-  lighter: '#F3F3F3',
-  light: '#DAE1E7',
-  dark: '#444',
-  black: '#000',
+const uiTheme = {
+  palette: {
+    primaryColor: COLOR.green500,
+  },
+  colors: {
+    primary: '#1292B4',
+    white: '#FFF',
+    lighter: '#F3F3F3',
+    light: '#DAE1E7',
+    dark: '#444',
+    black: '#000',
+  },
 };
 
-// store results in a context
-// home screen with get started - layer 1
-// ask for number of translations desired - layer 2
-// ask for input - layer 3
-
-// show results - modal
-
-const App = () => (
-  <>
-    <StatusBar barStyle="dark-content" />
-    <SafeAreaView>
-      <ScrollView
-        style={styles.scrollView}
-        contentContainerStyle={styles.scrollViewContainer}
-      >
-        <View style={styles.body}>
-          <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>Welcome To Operator</Text>
-            <Text style={styles.sectionDescription}>
-              Operator is a digital version of the game telephone.
-            </Text>
-          </View>
-          <Button
-            style={{
-              container: styles.button,
-              text: styles.buttonText,
-            }}
-            text="Get Started!"
-            primary
-            raised
-          />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  </>
+const AppNavigator = createStackNavigator(
+  {
+    Home,
+    NumberOfCalls,
+  },
+  {
+    initialRouteName: 'Home',
+  },
 );
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.white,
-    height: '100%',
-  },
-  scrollViewContainer: {
-    display: 'flex',
-    flex: 1,
-  },
-  body: {
-    flex: 1,
-    display: 'flex',
-  },
-  sectionContainer: {
-    marginTop: 200,
-    paddingHorizontal: 24,
-    flex: 5,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  button: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonText: {
-    fontSize: 22,
-  },
-});
+const AppContainer = createAppContainer(AppNavigator);
+
+const App = () => (
+  <ThemeContext.Provider value={getTheme(uiTheme)}>
+    <StatusBar barStyle="dark-content" />
+    <AppContainer />
+  </ThemeContext.Provider>
+);
 
 export default App;
---
+++ screens/Home.js
@@ -0,0 +1,101 @@
+import React, { useCallback } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+} from 'react-native';
+import { Button } from 'react-native-material-ui';
+
+// store results in a context
+// home screen with get started - layer 1
+// ask for number of translations desired - layer 2
+// ask for input - layer 3
+
+// show results - modal
+
+const Colors = {
+  primary: '#1292B4',
+  white: '#FFF',
+  lighter: '#F3F3F3',
+  light: '#DAE1E7',
+  dark: '#444',
+  black: '#000',
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.white,
+    height: '100%',
+  },
+  scrollViewContainer: {
+    display: 'flex',
+    flex: 1,
+  },
+  body: {
+    flex: 1,
+    display: 'flex',
+  },
+  sectionContainer: {
+    marginTop: 20,
+    paddingHorizontal: 24,
+    flex: 5,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 22,
+  },
+});
+
+const HomeScreen = ({ navigation }) => {
+  const goToNumberOfCalls = useCallback(() => navigation.navigate('NumberOfCalls'), [navigation]);
+
+  return (
+    <SafeAreaView>
+      <ScrollView
+        style={styles.scrollView}
+        contentContainerStyle={styles.scrollViewContainer}
+      >
+        <View style={styles.body}>
+          <View style={styles.sectionContainer}>
+            <Text style={styles.sectionDescription}>
+              Operator is a digital version of the game telephone.
+            </Text>
+          </View>
+          <Button
+            onPress={goToNumberOfCalls}
+            style={{
+              container: styles.button,
+              text: styles.buttonText,
+            }}
+            text="Get Started!"
+            primary
+            raised
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+HomeScreen.navigationOptions = {
+  title: 'Welcome to Operator!',
+};
+
+export default HomeScreen;
